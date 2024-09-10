5983d33f90ec7406080a7e808a04bbc20a5177bc
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-10 18:44:55 +0300

    Add files via upload

---
+++ 00015-medium-last.ts
@@ -0,0 +1,7 @@
+type Last<T extends any[]> = T extends [...any[], infer Last] ? Last : never;
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type tail1 = Last<arr1> // expected to be 'c'
+type tail2 = Last<arr2> // expected to be 1
