e68d237df1c1fba3718a336ba836a32b4b0bfb01
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-13 21:54:28 +0300

    Add files via upload

---
+++ 03312-easy-parameters.ts
@@ -0,0 +1,4 @@
+type MyParameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+
+const foo = (arg1: string, arg2: number): void => {}
+type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]
